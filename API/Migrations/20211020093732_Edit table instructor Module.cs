604b9b32d93dfb3da0eafd7995c7012ba01283f2
Ahmed abdel Ghafar Ali <AhmedG@hits-egypt.local>
2021-10-20 13:10:45 +0200

    add Skill Instructor Module

---
+++ API/Migrations/20211020093732_Edit table instructor Module.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class EdittableinstructorModule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseModuleId",
+                table: "Modules");
+
+            migrationBuilder.DropColumn(
+                name: "CourseInstructorId",
+                table: "Instructors");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CourseModuleId",
+                table: "Modules",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CourseInstructorId",
+                table: "Instructors",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
