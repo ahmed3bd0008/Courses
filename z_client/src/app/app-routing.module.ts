b7ae006d3d182da8384412599949c37170f3d1b2
ahmed3bdo008 <ahmed3bdo008@gmail.com>
2021-10-28 14:47:44 -0700

    add approuter

--- z_client/src/app/app-routing.module.ts
+++ z_client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
+import { courselistcomponent } from './Courses/Course-list/course-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { Usercomponent } from './User/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'courses',component:courselistcomponent},
+  {path:'user',component:Usercomponent},
+  {path:'',component:courselistcomponent,pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
